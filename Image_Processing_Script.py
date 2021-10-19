02c7792da304e0fef17a0167f8331345c1e14a49
JonathanTaylor9 <65346162+JonathanTaylor9@users.noreply.github.com>
2021-10-19 08:48:38 +0100

    Removed empty line 15

--- Image_Processing_Script.py
+++ Image_Processing_Script.py
@@ -8,7 +8,7 @@ resized_image = cv2.resize(img, (int(img.shape[1] / 2), int(img.shape[0] / 2)))
 cv2.imshow("Galaxy", resized_image)
 # saves resized image as 'Galaxy_Resized.jpg'
 cv2.imwrite("Galaxy_Resized.jpg", resized_image)
-# possibility to show image for period of time - currently permanently
+# possibility to show image for period of time - currently displays until closed by user
 cv2.waitKey(0)
 # closes image window; used in conjunction with waitKey command above
 cv2.destroyAllWindows()
